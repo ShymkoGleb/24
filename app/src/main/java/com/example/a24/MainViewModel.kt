a864d9e5fa73cd3266bbae9fab8e823c1e055432
ShymkoGleb <71500261+ShymkoGleb@users.noreply.github.com>
2021-02-14 22:04:16 +0200

    xml dimen added

---
+++ app/src/main/java/com/example/a24/MainViewModel.kt
@@ -0,0 +1,46 @@
+package com.example.a24
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a24.a1_DataSource.mapper.UsersDataPostToModelMapper
+import com.example.a24.a1_DataSource.repository.impl.UserPostDatasorceImpl
+import com.example.a24.a1_DataSource.source.exe.UsersPostFromJsonExe
+import com.example.a24.a2_Domain.UserRepository
+import com.example.a24.a2_Domain.mapper.UserPostDomainToUIMapper
+import com.example.a24.a2_Domain.useCase.UserPostGetUseCase
+import com.example.a24.a3_UI.modelUi.UsersPostUIModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainViewModel : ViewModel() {
+
+    val reposUsersPostsLiveData = MutableLiveData<List<UsersPostUIModel>>()
+
+    val userPostGetUseCasee: UserPostGetUseCase = UserPostGetUseCase(
+        UserPostDatasorceImpl(
+            UsersPostFromJsonExe(
+                Retrofit.Builder()
+                    .baseUrl("https://jsonplaceholder.typicode.com")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build(
+                    )
+            ), UsersDataPostToModelMapper()
+        ), UserPostDomainToUIMapper(UserRepository())
+    )
+
+    fun getUsersPost(){
+        viewModelScope.launch(Dispatchers.IO) {
+            val result = userPostGetUseCasee.execute()
+            withContext(Dispatchers.Main) {
+                result?.also { repos ->
+                    reposUsersPostsLiveData.postValue(repos)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a24/a2_Domain/mapper/UserPostDomainToUIMapper.kt
@@ -0,0 +1,43 @@
+package com.example.a24.a2_Domain.mapper
+
+import com.example.a24.a1_DataSource.model.UsersPostDataSourceModel
+import com.example.a24.a2_Domain.UserRepository
+import com.example.a24.a2_Domain.UserStatus
+import com.example.a24.a3_UI.modelUi.UsersPostUIModel
+
+class UserPostDomainToUIMapper(
+    private val usersRepository: UserRepository
+) {
+    fun map(models: List<UsersPostDataSourceModel>?): List<UsersPostUIModel>? {
+
+
+//        val bannedUsers = usersRepository.getUsersWithBan()
+//        val userWithWarning = usersRepository.getUsersWithWarning()
+
+        return models?.let {
+            models.map {
+                if (it.userId in UserRepository.ListOfIdForUserWithWarning) {
+                    UsersPostUIModel(
+                        it.userId,
+                        it.title,
+                        it.body,
+                        UserStatus.WARNING
+                    )
+                } else if (it.userId in UserRepository.ListOfIdForUserWithBann) {
+                    UsersPostUIModel(
+                        it.userId,
+                        " ",
+                        "Post from User ${it.userId} has been BANNED",
+                        UserStatus.BANNED
+                    )
+                } else UsersPostUIModel(
+                    it.userId,
+                    it.title,
+                    it.body,
+                    UserStatus.NORMAL
+                )
+            }
+        }
+    }
+}
+
